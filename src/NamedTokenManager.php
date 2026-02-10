30d279256a03609b0d73c632940884f6d8333442
Patrick Morpey <patrick@morphp.nl>
2026-02-10 16:17:28 +0100

    Add debug event dispatch for invalid token handling in NamedTokenManager and bump version to 2.0.3

--- src/NamedTokenManager.php
+++ src/NamedTokenManager.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Medas\ApiKeys;
 
-use Medas\Core\{Attributes\Service, Interfaces\BearerTokenValidator};
+use Medas\Core\{Attributes\Service, Events\DebugInformation, Interfaces\BearerTokenValidator};
 
 #[Service]
 readonly class NamedTokenManager implements BearerTokenValidator
@@ -45,12 +45,16 @@ readonly class NamedTokenManager implements BearerTokenValidator
     public function userId(string $token): string|null
     {
         if (substr_count($token, ':') !== 1) {
+            dispatch(new DebugInformation('[named-token-manager] token does not contain exactly one colon'));
+
             return null;
         }
 
         [$name, $key] = explode(':', $token);
 
         if (!$this->validator->validate($name, $key)) {
+            dispatch(new DebugInformation('[named-token-manager] key "%s" is invalid for name "%s"', $key, $name));
+
             return null;
         }
 
