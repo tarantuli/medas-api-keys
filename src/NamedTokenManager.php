38824b84de61561e3fa6e28039c44d835a189494
Patrick Morpey <patrick@morphp.nl>
2026-04-30 11:38:05 +0200

    Refactored `NamedTokenManager` token handling methods and updated dependencies in `composer.lock`.

--- src/NamedTokenManager.php
+++ src/NamedTokenManager.php
@@ -8,13 +8,15 @@ use Medas\Core\{
     Attributes\ConfigValue,
     Attributes\Service,
     Events\DebugInformation,
-    Interfaces\BearerTokenValidator
+    Interfaces\AuthenticationTokenController
 };
+use Medas\Json\JsonEncoder;
 
 #[Service]
-readonly class NamedTokenManager implements BearerTokenValidator
+readonly class NamedTokenManager implements AuthenticationTokenController
 {
     public function __construct(
+        private JsonEncoder     $jsonEncoder,
         private KeyCreator      $keyCreator,
         private KeyStoreManager $keyStoreManager,
         private Validator       $validator,
@@ -25,16 +27,17 @@ readonly class NamedTokenManager implements BearerTokenValidator
     {
     }
 
-    public function create(string $name): string
+    public function create(mixed $data): string
     {
         $key = $this->keyCreator->create();
+        $name = $this->jsonEncoder->decode($data);
 
         $this->keyStoreManager->storeKey($name, $key);
 
         return "$name:$key";
     }
 
-    public function delete(string $token): void
+    public function invalidate(string $token): void
     {
         if (!$this->userId($token)) {
             return;
@@ -50,7 +53,7 @@ readonly class NamedTokenManager implements BearerTokenValidator
         }
     }
 
-    public function userId(string $token): string|null
+    public function data(string $token): string|null
     {
         if (substr_count($token, ':') !== 1) {
             dispatch(new DebugInformation('[named-token-manager] token does not contain exactly one colon'));
@@ -77,6 +80,12 @@ readonly class NamedTokenManager implements BearerTokenValidator
             ));
         }
 
-        return $name;
+        return $this->jsonEncoder->decode($name);
+    }
+
+    public function userId(string $token): string|null
+    {
+        // The whole data result is the user ID by definition
+        return $this->data($token);
     }
 }
